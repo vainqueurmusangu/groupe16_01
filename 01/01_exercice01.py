3c30abac08f8b73b40d3764f56cbc0cf88ff4bfd
Hattie-Kwamba <157996053+Hattie-Kwamba@users.noreply.github.com>
2025-07-15 12:39:09 +0100

    premier

---
+++ 01/01_exercice01.py
@@ -0,0 +1,22 @@
+# Demande d'information à l'utilisateur
+
+try:
+    prenom = str(input("Entrer votre prénom svp: "))
+    age = int(input("Entrer votre âge svp: "))
+    ville = str(input("Entrer votre ville svp: "))
+    metier = str(input("Entrez votre métier svp: "))
+
+    # Approximation des jours vécus
+
+    jours_vecu = age * 365
+
+    # Affichage formaté
+
+    print(f'\n==== PROFIL UTILISATEUR ====')
+    print(f'Prénom: {prenom}')
+    print(f'Âge: {age} ans {jours_vecu} jours vécus environ')
+    print(f'Ville: {ville}')
+    print(f'Métier: {metier}')
+except ValueError:
+    print(f'Veillez saisir les données valides svp!')
+
